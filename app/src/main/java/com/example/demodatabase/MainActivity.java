2617706a3bdadbf876867a02144f01cff341f1e3
17045697 <17045697@myrp.edu.sg>
2019-05-09 12:52:54 +0800

    Initial commit

---
+++ app/src/main/java/com/example/demodatabase/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.demodatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert;
+    Button btnGetTasks;
+    TextView tvResults;
+    ListView lv;
+    ArrayAdapter aa;
+    ArrayList<Task> task;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInsert = findViewById(R.id.buttonInsert);
+        btnGetTasks = findViewById(R.id.buttonGetTasks);
+        tvResults = findViewById(R.id.textView);
+        lv = findViewById(R.id.lvTasks);
+        task = new ArrayList<Task>();
+        aa = new TaskAdapter(this,R.layout.row, task);
+        DMHelper db = new DMHelper(MainActivity.this);
+        ArrayList<Task> data = db.getTasks();
+        aa = new TaskAdapter(this,R.layout.row,data);
+        lv.setAdapter(aa);
+
+        btnInsert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DMHelper db = new DMHelper(MainActivity.this);
+                db.insertTask("Submit RJ", "25 Apr 2016");
+                db.close();
+            }
+        });
+
+        btnGetTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DMHelper db = new DMHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getTaskContent();
+                db.close();
+
+                String txt = "";
+                for (int i = 0; i < data.size(); i++) {
+                    Log.d("Database Content", i + ". " + data.get(i));
+                    txt += i + ". " + data.get(i) + "\n";
+                }
+                tvResults.setText(txt);
+            }
+        });
+    }
+}
